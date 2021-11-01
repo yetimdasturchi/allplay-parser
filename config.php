d54cc3d3c02a4b3718d70bf6194dc2b5e7109063
Manuchehr Usmonov <34451515+con9799@users.noreply.github.com>
2021-11-01 18:40:43 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $config['auth'] = [
-    'email'     => 'con9799@mail.ru',
-    'password'  => '19970623m',
-    'device_id' => '5hdeeeeerdi00r01y82cidu',
+    'email'     => '',
+    'password'  => '',
+    'device_id' => '5qabbbberdi01r02y83cidu',
 ];
 
 $config['channels'] = json_decode(file_get_contents(__DIR__.'/channels.json'),TRUE);
